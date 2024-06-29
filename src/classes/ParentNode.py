aedb46a17343b62500368c8ae228673672993341
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-06-29 11:36:55 -0700

    feat: Add ParentNode handling

---
+++ src/classes/ParentNode.py
@@ -0,0 +1,28 @@
+from src.classes.HTMLNode import HTMLNode
+from typing import Optional
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: Optional[str] = None, children: list[HTMLNode] | None = None, props: Optional[dict[str, str | bool | int]] = None):
+        super().__init__(tag, children=children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("No tags provided")
+        if not self.children:
+            raise ValueError("No children provided")
+
+        s: list[str] = []
+
+        if self.props:
+            s.append(f"<{self.tag} {self.props_to_html()}>")
+        else:
+            s.append(f"<{self.tag}>")
+
+        for child in self.children:
+            s.append(child.to_html())
+
+        s.append(f"</{self.tag}>")
+
+        return "".join(s)
+
---
+++ src/tests/test_parentnode.py
@@ -0,0 +1,119 @@
+import unittest
+
+from src.classes.ParentNode import ParentNode
+from src.classes.LeafNode import LeafNode
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+
+        expectedHTML = "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>"
+
+        self.assertEqual(node.to_html(), expectedHTML)
+
+    def test_nested_nodes(self):
+        node = ParentNode(
+            "div",
+            [
+                ParentNode(
+                    "p",
+                    [
+                        LeafNode("b", "Bold Text"),
+                        LeafNode(None, "Normal Text")
+                    ]
+                ),
+                LeafNode("i", "italic text")
+            ]
+        )
+
+        expectedHTML = "<div><p><b>Bold Text</b>Normal Text</p><i>italic text</i></div>"
+
+        self.assertEqual(node.to_html(), expectedHTML)
+
+    def test_parent_nodes_no_leafs(self):
+        node = ParentNode(
+            "div",
+            [
+                ParentNode(
+                    "p"
+                )
+            ]
+        )
+
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_empty_parent_node(self):
+        node = ParentNode("div")
+
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_node_without_text(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b"),
+                LeafNode(None),
+                LeafNode("i"),
+                LeafNode(None),
+            ],
+        )
+
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_parent_node_with_attributes(self):
+        node = ParentNode("div", [LeafNode("span", "Some text")], {"class": "container", "id": "main"})
+        expectedHTML = '<div class="container" id="main"><span>Some text</span></div>'
+        self.assertEqual(node.to_html(), expectedHTML)
+
+    def test_mixed_nested_and_leaf_nodes(self):
+            node = ParentNode(
+                "div",
+                [
+                    ParentNode(
+                        "p",
+                        [
+                            LeafNode("b", "Bold Text"),
+                            ParentNode(
+                                "span",
+                                [
+                                    LeafNode(None, "Span text")
+                                ]
+                            ),
+                            LeafNode(None, "Normal Text")
+                        ]
+                    ),
+                    LeafNode("i", "italic text")
+                ]
+            )
+            expectedHTML = "<div><p><b>Bold Text</b><span>Span text</span>Normal Text</p><i>italic text</i></div>"
+            self.assertEqual(node.to_html(), expectedHTML)
+
+
+    def test_node_with_no_tag(self):
+        node = ParentNode()
+
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_single_leaf_node(self):
+        node = ParentNode(
+            "a",
+            [
+                LeafNode("b", "Bold Text")
+            ]
+        )
+
+        expectedHTML = "<a><b>Bold Text</b></a>"
+
+        self.assertEqual(node.to_html(), expectedHTML)
