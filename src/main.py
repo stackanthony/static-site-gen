7e417c8048954494871cbc442080659520681b95
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-07-12 08:54:07 -0700

    chore: clean up main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,1 @@
-from classes.TextNode import TextNode
-def main():
-    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev/")
-    print(text_node)
-main()
+# TODO: Work on this
