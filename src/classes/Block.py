8bd777a6e8e4a77a38128c803e4ec819dd33241a
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-07-16 17:00:28 -0700

    feat: add markdown to blocks and Block object

---
+++ src/classes/Block.py
@@ -0,0 +1,18 @@
+class Block:
+    def __init__(self, text: str) -> None:
+        self.text = text
+
+    def __eq__(self, other: object, /) -> bool:
+        if not isinstance(other, Block):
+            return NotImplemented
+        return self.text == other.text
+
+    @staticmethod
+    def markdown_to_blocks(markdown: str) -> list['Block']:
+        blocks: list['Block'] = []
+        split_text: list[str] = markdown.split("\n\n")
+
+        for text in split_text:
+            blocks.append(Block(text.strip()))
+
+        return blocks
---
+++ src/tests/test_block.py
@@ -0,0 +1,23 @@
+import unittest
+
+from src.classes.Block import Block
+
+class TestBlock(unittest.TestCase):
+    def test_eq(self):
+        block = Block("test")
+        block2 = Block("test")
+
+        self.assertEqual(block, block2)
+
+    def test_markdown_to_blocks(self):
+        markdown = "# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+
+        expected_return_list = [
+            "# This is a heading",
+            "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+            "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
+        ]
+
+        return_list = [block.text for block in Block.markdown_to_blocks(markdown)]
+        self.assertListEqual(return_list, expected_return_list)
+
