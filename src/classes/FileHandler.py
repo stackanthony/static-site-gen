4fed78b92c3173b6e8df8b182eafe0f5adf7040f
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-07-29 11:37:27 -0700

    feat: Add FileHandler supports copying, deleting, creating files/dirs

--- src/classes/FileHandler.py
+++ src/classes/FileHandler.py
@@ -6,12 +6,40 @@ class FileHandler:
     @staticmethod
     def copy_files(source_dir: str, dest_dir: str) -> bool:
         # clean destination directory
-        FileHandler.delete_files(dest_dir)
+        source_dir_entries: list[str] = os.listdir(source_dir)
+        if source_dir_entries:
+            FileHandler.delete_files(dest_dir)
+
+        for file_name in source_dir_entries:
+            source_path = os.path.join(source_dir, file_name)
+            dest_path = os.path.join(dest_dir, file_name)
+
+            try:
+                if os.path.isfile(source_path) and os.path.exists(dest_dir):
+                    shutil.copy(source_path, dest_path)
+                    continue
+
+                FileHandler.create_dir(dest_path)
+                FileHandler.copy_files(source_path, dest_path)
+            except Exception as err:
+                print(f"Failed to copy files from {source_path} to {dest_path}. Reason: {err}")
+                return False
 
         return True
 
+
     @staticmethod
-    def delete_files(dir: str):
+    def create_dir(path: str) -> bool:
+        try:
+            os.mkdir(path)
+        except Exception as err:
+            print(f"Unable to create directory. Reason: {err}")
+            return False
+        
+        return True
+
+    @staticmethod
+    def delete_files(dir: str) -> bool:
         for file_name in os.listdir(dir):
             file_path = os.path.join(dir, file_name)
             try:
@@ -21,4 +49,7 @@ class FileHandler:
                     shutil.rmtree(file_path)
             except Exception as err:
                 print(f"Failed to delete {file_path}. Reason: {err}")
+                return False
+        
+        return True
 
--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-# TODO: Work on this
+from classes.FileHandler import FileHandler
+
+
+def main():
+    FileHandler.copy_files("../static/", "../public")
+
+if __name__ == "__main__":
+    main()
