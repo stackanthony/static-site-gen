6d167c5c5e57ddad56c2fd3b0c8e21d3fa033d9a
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-07-30 11:17:21 -0700

    fix: Instead of returning default HTMLNode now returns ParentNode if it's parent

--- src/classes/MarkdownProcessor.py
+++ src/classes/MarkdownProcessor.py
@@ -1,5 +1,7 @@
 from src.classes.Block import Block
 from src.classes.HTMLNode import HTMLNode
+from src.classes.LeafNode import LeafNode
+from src.classes.ParentNode import ParentNode
 from src.classes.TextNode import TextNode
 from src.classes.blocks.BlockFactory import BlockFactory
 from src.classes.blocks.Code import Code
@@ -13,53 +15,45 @@ class MarkdownProcessor:
     def __init__(self, markdown: str):
         self.markdown = markdown
 
-    def extract_title(self) -> str:
-        split_markdown: list[str] = self.markdown.split("#", maxsplit=1)
-        if len(split_markdown) < 2 or split_markdown[0] != "" or split_markdown[1].count("#"):
-            raise Exception("No Title")
-
-        return split_markdown[1].strip()
-
-
-    def markdown_to_html_node(self):
+    def markdown_to_html_node(self) -> HTMLNode:
         blocks: list[Block] = BlockFactory.create_blocks(self.markdown)
-        root_node: HTMLNode = HTMLNode("div")
+        root_node: ParentNode = ParentNode("div")
 
         for block in blocks:
-            new_node: HTMLNode = HTMLNode()
+            new_node: ParentNode | None = None
             if isinstance(block, Heading):
-                new_node = HTMLNode(f"h{block.count}", children=[
+                new_node = ParentNode(f"h{block.count}", children=[
                     TextNode(block.text, "text").text_node_to_html_node()
                 ])
                 root_node.children.append(new_node)
             elif isinstance(block, Code):
-                new_node = HTMLNode("pre", children=[
+                new_node = ParentNode("pre", children=[
                     TextNode(block.text, "code").text_node_to_html_node()
                 ])
                 root_node.children.append(new_node)
             elif isinstance(block, Quote):
-                new_node = HTMLNode("blockquote", children=[
+                new_node = ParentNode("blockquote", children=[
                     TextNode(block.text, "text").text_node_to_html_node()
                 ])
                 root_node.children.append(new_node)
             elif isinstance(block, UnorderedList):
-                root_list_node = HTMLNode("ul")
+                root_list_node = ParentNode("ul")
 
                 for item in block.items:
-                    child_list_node = HTMLNode("li", item)
+                    child_list_node = LeafNode("li", item)
                     root_list_node.children.append(child_list_node)
 
                 root_node.children.append(root_list_node)
             elif isinstance(block, OrderedList):
-                root_list_node = HTMLNode("ol")
+                root_list_node = ParentNode("ol")
 
                 for item in block.items:
-                    child_list_node = HTMLNode("li", item)
+                    child_list_node = LeafNode("li", item)
                     root_list_node.children.append(child_list_node)
 
                 root_node.children.append(root_list_node)
             else:
-                new_node = HTMLNode("p", children=[
+                new_node = ParentNode("p", children=[
                     TextNode(block.text, "text").text_node_to_html_node()
                 ])
 
