d0cfec7f503d8b48f9496472203accfc191ff5f8
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-07-31 15:01:20 -0700

    fix: leafnode children being set to their appropriate type instead of just "text"

--- src/classes/MarkdownProcessor.py
+++ src/classes/MarkdownProcessor.py
@@ -17,46 +17,54 @@ class MarkdownProcessor:
 
     def markdown_to_html_node(self) -> HTMLNode:
         blocks: list[Block] = BlockFactory.create_blocks(self.markdown)
-        root_node: ParentNode = ParentNode("div")
+        root_node: ParentNode = ParentNode('div')
 
         for block in blocks:
             new_node: ParentNode | None = None
             if isinstance(block, Heading):
-                new_node = ParentNode(f"h{block.count}", children=[
-                    TextNode(block.text, "text").text_node_to_html_node()
-                ])
+                new_node = ParentNode(f'h{block.count}')
+                new_node.children = [
+                    text_node.text_node_to_html_node()
+                    for text_node in TextNode.text_to_textnodes(block.text)
+                ]
                 root_node.children.append(new_node)
             elif isinstance(block, Code):
-                new_node = ParentNode("pre", children=[
-                    TextNode(block.text, "code").text_node_to_html_node()
-                ])
+                new_node = ParentNode(
+                    'pre',
+                    children=[
+                        TextNode(block.text, 'code').text_node_to_html_node()
+                    ],
+                )
                 root_node.children.append(new_node)
             elif isinstance(block, Quote):
-                new_node = ParentNode("blockquote", children=[
-                    TextNode(block.text, "text").text_node_to_html_node()
-                ])
+                new_node = ParentNode('blockquote')
+                new_node.children = [
+                    text_node.text_node_to_html_node()
+                    for text_node in TextNode.text_to_textnodes(block.text)
+                ]
                 root_node.children.append(new_node)
             elif isinstance(block, UnorderedList):
-                root_list_node = ParentNode("ul")
+                root_list_node = ParentNode('ul')
 
                 for item in block.items:
-                    child_list_node = LeafNode("li", item)
+                    child_list_node = LeafNode('li', item)
                     root_list_node.children.append(child_list_node)
 
                 root_node.children.append(root_list_node)
             elif isinstance(block, OrderedList):
-                root_list_node = ParentNode("ol")
+                root_list_node = ParentNode('ol')
 
                 for item in block.items:
-                    child_list_node = LeafNode("li", item)
+                    child_list_node = LeafNode('li', item)
                     root_list_node.children.append(child_list_node)
 
                 root_node.children.append(root_list_node)
             else:
-                new_node = ParentNode("p", children=[
-                    TextNode(block.text, "text").text_node_to_html_node()
-                ])
-
+                new_node = ParentNode('p')
+                new_node.children = [
+                    text_node.text_node_to_html_node()
+                    for text_node in TextNode.text_to_textnodes(block.text)
+                ]
                 root_node.children.append(new_node)
 
         return root_node
