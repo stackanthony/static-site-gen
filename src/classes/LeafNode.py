ee87cdff84b8b78fe57c5c3a885dd7161e98ed23
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-07-31 14:59:21 -0700

    chore: apply formatting

--- src/classes/LeafNode.py
+++ src/classes/LeafNode.py
@@ -1,19 +1,29 @@
 from src.classes.HTMLNode import HTMLNode
 from typing import Optional
 
+
 class LeafNode(HTMLNode):
-    def __init__(self, tag: Optional[str] = None, value: Optional[str] = None, props: Optional[dict[str, str]] = None):
+    def __init__(
+        self,
+        tag: Optional[str] = None,
+        value: str = '',
+        props: Optional[dict[str, str]] = None,
+    ):
         super().__init__(tag, value, props=props)
 
+    def __str__(self) -> str:
+        return f'Tag: {self.tag}\nValue: [{self.value}]\nProps: {self.props}\n'
+
     def to_html(self) -> str:
-        if not self.value and self.tag != "img":
+        if not self.value and self.tag != 'img':
             raise ValueError
 
-        if not self.tag or self.tag == "text":
-            return self.value if self.value else ""
+        if not self.tag or self.tag == 'text':
+            return self.value if self.value else ''
 
         if not self.props:
-            return f"<{self.tag}>{self.value}</{self.tag}>"
+            return f'<{self.tag}>{self.value}</{self.tag}>'
 
-        return f"<{self.tag} {super().props_to_html()}>{self.value}</{self.tag}>"
-
+        return (
+            f'<{self.tag} {super().props_to_html()}>{self.value}</{self.tag}>'
+        )
