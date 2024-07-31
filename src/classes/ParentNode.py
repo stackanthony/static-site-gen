6cb58f867119ccd4efe076d0ed6b9bfc23c34482
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-07-31 15:00:24 -0700

    chore: apply formatting & small optimizations

--- src/classes/ParentNode.py
+++ src/classes/ParentNode.py
@@ -3,26 +3,30 @@ from typing import Optional
 
 
 class ParentNode(HTMLNode):
-    def __init__(self, tag: Optional[str] = None, children: Optional[list[HTMLNode]] = None, props: Optional[dict[str, str]] = None):
+    def __init__(
+        self,
+        tag: Optional[str] = None,
+        children: Optional[list[HTMLNode]] = None,
+        props: Optional[dict[str, str]] = None,
+    ):
         super().__init__(tag, children=children, props=props)
 
-    def to_html(self) -> str:
+    def to_html(self):
         if not self.tag:
-            raise ValueError("No tags provided")
+            raise ValueError('No tags provided')
         if not self.children:
-            raise ValueError("No children provided")
+            raise ValueError('No children provided')
 
         s: list[str] = []
 
         if self.props:
-            s.append(f"<{self.tag} {self.props_to_html()}>")
+            s.append(f'<{self.tag} {self.props_to_html()}>')
         else:
-            s.append(f"<{self.tag}>")
+            s.append(f'<{self.tag}>')
 
         for child in self.children:
             s.append(child.to_html())
 
-        s.append(f"</{self.tag}>")
+        s.append(f'</{self.tag}>')
 
-        return "".join(s)
-
+        return ''.join(s)
--- src/classes/TextNode.py
+++ src/classes/TextNode.py
@@ -3,8 +3,11 @@ from typing import Optional
 from src.classes.LeafNode import LeafNode
 from src.classes.TextProcessor import TextProcessor
 
+
 class TextNode:
-    def __init__(self, text: str="", text_type: str="", url: Optional[str] = None) -> None:
+    def __init__(
+        self, text: str = '', text_type: str = '', url: Optional[str] = None
+    ) -> None:
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -12,47 +15,58 @@ class TextNode:
     def __eq__(self, other: object, /) -> bool:
         if not isinstance(other, TextNode):
             return NotImplemented
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
 
     def __repr__(self) -> str:
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
-    
+        return f'TextNode({self.text}, {self.text_type}, {self.url})'
+
     def text_node_to_html_node(self) -> LeafNode:
         match self.text_type:
-            case "text":
-                return LeafNode("text", self.text)
-            case "bold":
-                return LeafNode("b", self.text)
-            case "italic":
-                return LeafNode("i", self.text)
-            case "code":
-                return LeafNode("code", self.text)
-            case "link":
+            case 'text':
+                return LeafNode('text', self.text)
+            case 'bold':
+                return LeafNode('b', self.text)
+            case 'italic':
+                return LeafNode('i', self.text)
+            case 'code':
+                return LeafNode('code', self.text)
+            case 'link':
                 if not self.url:
-                    raise ValueError("No URL to parse")
-                return LeafNode("a", self.text, {"href": self.url})
-            case "image":
+                    raise ValueError('No URL to parse')
+                return LeafNode('a', self.text, {'href': self.url})
+            case 'image':
                 if not self.url:
-                    raise ValueError("No Image URL in Node")
-                return LeafNode("img", "", {"src": self.url})
+                    raise ValueError('No Image URL in Node')
+                return LeafNode('img', '', {'src': self.url})
             case _:
-                raise Exception("Not a valid text type. Couldn't convert to LeafNode")
+                raise Exception(
+                    "Not a valid text type. Couldn't convert to LeafNode"
+                )
 
     @staticmethod
     def text_to_textnodes(text: str) -> list['TextNode']:
-        text_nodes: list['TextNode'] = [TextNode(text, "text")]
-
         if text == '':
             return []
 
+        text_nodes: list['TextNode'] = [TextNode(text, 'text')]
         try:
-            for bold_node in TextNode.split_nodes_delimiter([text_nodes.pop()], "**", "bold"):
+            for bold_node in TextNode.split_nodes_delimiter(
+                [text_nodes.pop()], '**', 'bold'
+            ):
                 text_nodes.append(bold_node)
 
-            for italic_node in TextNode.split_nodes_delimiter([text_nodes.pop()], "*", "italic"):
+            for italic_node in TextNode.split_nodes_delimiter(
+                [text_nodes.pop()], '*', 'italic'
+            ):
                 text_nodes.append(italic_node)
 
-            for code_node in TextNode.split_nodes_delimiter([text_nodes.pop()], "`", "code"):
+            for code_node in TextNode.split_nodes_delimiter(
+                [text_nodes.pop()], '`', 'code'
+            ):
                 text_nodes.append(code_node)
 
             for image_node in TextNode.split_nodes_image([text_nodes.pop()]):
@@ -64,22 +78,23 @@ class TextNode:
             pass
 
         return text_nodes
-        
 
     @staticmethod
-    def split_nodes_delimiter(old_nodes: list['TextNode'], delimeter: str, text_type: str) -> list['TextNode']:
+    def split_nodes_delimiter(
+        old_nodes: list['TextNode'], delimeter: str, text_type: str
+    ) -> list['TextNode']:
         new_nodes: list['TextNode'] = []
         text_types: dict[str, str] = {
-            "bold" : "**",
-            "italic": "*",
-            "code": "`",
+            'bold': '**',
+            'italic': '*',
+            'code': '`',
         }
 
-        if text_type not in text_types: 
-            raise ValueError(f"{text_type} - not a valid text type to convert")
+        if text_type not in text_types:
+            raise ValueError(f'{text_type} - not a valid text type to convert')
 
         if delimeter not in text_types.values():
-            raise ValueError(f"{delimeter} - not a valid delimeter to convert")
+            raise ValueError(f'{delimeter} - not a valid delimeter to convert')
 
         for node in old_nodes:
             split_nodes_text: list[str] = node.text.split(delimeter)
@@ -87,8 +102,10 @@ class TextNode:
             if len(split_nodes_text) > 2:
                 # valid delimeter enclosed word
                 for i, split_node_text in enumerate(split_nodes_text):
+                    if not split_node_text:
+                        continue
                     new_node = TextNode(split_node_text)
-                    new_node.text_type = "text" if i % 2 == 0 else text_type
+                    new_node.text_type = 'text' if i % 2 == 0 else text_type
                     new_nodes.append(new_node)
 
         if not new_nodes:
@@ -96,40 +113,44 @@ class TextNode:
 
         return new_nodes
 
-
     @staticmethod
     def split_nodes_image(old_nodes: list['TextNode']) -> list['TextNode']:
         new_nodes: list['TextNode'] = []
 
         for node in old_nodes:
             if TextProcessor(node.text).extract_markdown_images():
-                split_nodes_text = node.text.split("!")
+                split_nodes_text = node.text.split('!')
 
                 for split_node_text in split_nodes_text:
                     try:
-                        if split_node_text == "":
+                        if split_node_text == '':
                             continue
 
-                        tp = TextProcessor("!" + split_node_text)
+                        tp = TextProcessor('!' + split_node_text)
                         extract_markdown_images = tp.extract_markdown_images()
 
                         if not extract_markdown_images:
-                            new_nodes.append(TextNode(split_node_text, "text"))
+                            new_nodes.append(TextNode(split_node_text, 'text'))
                             continue
 
-                        new_nodes.append(TextNode(extract_markdown_images[0][0], "image",extract_markdown_images[0][1]))
+                        new_nodes.append(
+                            TextNode(
+                                extract_markdown_images[0][0],
+                                'image',
+                                extract_markdown_images[0][1],
+                            )
+                        )
 
-                        trailing_text = tp.get_trailing_text(")")
+                        trailing_text = tp.get_trailing_text(')')
 
                         if not trailing_text:
                             continue
 
-                        new_nodes.append(TextNode(trailing_text, "text"))
+                        new_nodes.append(TextNode(trailing_text, 'text'))
                     except:
                         continue
             else:
                 new_nodes.append(node)
-
 
         return new_nodes
 
@@ -145,29 +166,32 @@ class TextNode:
                     new_nodes.append(node)
                     continue
 
-                split_nodes_text = node.text.split("[")
+                split_nodes_text = node.text.split('[')
 
-                
                 for split_node_text in split_nodes_text:
 
-                    tp = TextProcessor("[" + split_node_text)
+                    tp = TextProcessor('[' + split_node_text)
                     extract_markdown_links = tp.extract_markdown_links()
 
                     if not extract_markdown_links:
-                        new_nodes.append(TextNode(split_node_text, "text"))
+                        new_nodes.append(TextNode(split_node_text, 'text'))
                         continue
 
-                    new_nodes.append(TextNode(extract_markdown_links[0][0], "link", extract_markdown_links[0][1]))
+                    new_nodes.append(
+                        TextNode(
+                            extract_markdown_links[0][0],
+                            'link',
+                            extract_markdown_links[0][1],
+                        )
+                    )
 
-                    trailing_text = tp.get_trailing_text(")")
+                    trailing_text = tp.get_trailing_text(')')
 
                     if not trailing_text:
                         continue
 
-                    new_nodes.append(TextNode(trailing_text, "text"))
+                    new_nodes.append(TextNode(trailing_text, 'text'))
             except:
                 continue
 
         return new_nodes
-
-
