92221e740cdc789b62da55d62103c4344b918510
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-08-20 01:15:19 -0700

    feat: Add recursive page generator for multiple pages

--- src/classes/PageGenerator.py
+++ src/classes/PageGenerator.py
@@ -2,6 +2,8 @@ from classes.MarkdownProcessor import MarkdownProcessor
 from src.classes.FileHandler import FileHandler
 from src.classes.TextProcessor import TextProcessor
 
+import os
+
 
 class PageGenerator:
     def __init__(self) -> None:
@@ -27,3 +29,37 @@ class PageGenerator:
         FileHandler.replace_word_file(
             dest_path, dest_path, '{{ Content }}', markdown_html
         )
+
+    @staticmethod
+    def generate_pages_recursive(
+            dir_path_content: str, template_path: str, dest_path: str
+        ):
+            try:
+                dir_entries: list[str] = FileHandler.list_dir_entries(dir_path_content)
+
+                for dir_entry in dir_entries:
+                    cur_path = os.path.join(dir_path_content, dir_entry)
+
+                    if cur_path.endswith(".md") and FileHandler.is_file(cur_path):
+                        # Create the destination path for the HTML file
+                        new_file_dest: str = os.path.join(
+                            dest_path, cur_path.split("/")[-1].replace(".md", ".html")
+                        )
+                        
+                        print(cur_path)
+                        # Generate the page using the provided context
+                        PageGenerator.generate_page(cur_path, template_path, new_file_dest)
+                        continue
+
+                    # If it's a directory, create the directory in the destination and recurse
+                    new_dest_path = os.path.join(dest_path, cur_path.split("/")[-1])
+                    FileHandler.create_dir(new_dest_path)
+                    
+                    # Recursive call to process subdirectories
+                    PageGenerator.generate_pages_recursive(
+                        cur_path, template_path, new_dest_path
+                    )
+
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                raise
