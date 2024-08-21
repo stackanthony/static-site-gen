c4d42024965f007b765a5015ef61d2521d5440bb
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-08-21 09:55:06 -0700

    chore: rm debug print statements

--- src/classes/PageGenerator.py
+++ src/classes/PageGenerator.py
@@ -6,9 +6,6 @@ import os
 
 
 class PageGenerator:
-    def __init__(self) -> None:
-        pass
-
     @staticmethod
     def generate_page(from_path: str, template_path: str, dest_path: str):
         print(
@@ -32,34 +29,40 @@ class PageGenerator:
 
     @staticmethod
     def generate_pages_recursive(
-            dir_path_content: str, template_path: str, dest_path: str
-        ):
-            try:
-                dir_entries: list[str] = FileHandler.list_dir_entries(dir_path_content)
+        dir_path_content: str, template_path: str, dest_path: str
+    ):
+        try:
+            dir_entries: list[str] = FileHandler.list_dir_entries(
+                dir_path_content
+            )
 
-                for dir_entry in dir_entries:
-                    cur_path = os.path.join(dir_path_content, dir_entry)
+            for dir_entry in dir_entries:
+                cur_path = os.path.join(dir_path_content, dir_entry)
 
-                    if cur_path.endswith(".md") and FileHandler.is_file(cur_path):
-                        # Create the destination path for the HTML file
-                        new_file_dest: str = os.path.join(
-                            dest_path, cur_path.split("/")[-1].replace(".md", ".html")
-                        )
-                        
-                        print(cur_path)
-                        # Generate the page using the provided context
-                        PageGenerator.generate_page(cur_path, template_path, new_file_dest)
-                        continue
-
-                    # If it's a directory, create the directory in the destination and recurse
-                    new_dest_path = os.path.join(dest_path, cur_path.split("/")[-1])
-                    FileHandler.create_dir(new_dest_path)
-                    
-                    # Recursive call to process subdirectories
-                    PageGenerator.generate_pages_recursive(
-                        cur_path, template_path, new_dest_path
+                if cur_path.endswith('.md') and FileHandler.is_file(cur_path):
+                    # Create the destination path for the HTML file
+                    new_file_dest: str = os.path.join(
+                        dest_path,
+                        cur_path.split('/')[-1].replace('.md', '.html'),
                     )
 
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                raise
+                    # Generate the page using the provided context
+                    PageGenerator.generate_page(
+                        cur_path, template_path, new_file_dest
+                    )
+                    continue
+
+                # If it's a directory, create the directory in the destination and recurse
+                new_dest_path = os.path.join(
+                    dest_path, cur_path.split('/')[-1]
+                )
+                FileHandler.create_dir(new_dest_path)
+
+                # Recursive call to process subdirectories
+                PageGenerator.generate_pages_recursive(
+                    cur_path, template_path, new_dest_path
+                )
+
+        except Exception as e:
+            print(f'An error occurred: {e}')
+            raise
