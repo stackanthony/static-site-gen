b8913bb5b8fecfd4c37386cd8d153368e8f3d0d9
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-08-21 18:30:08 -0700

    chore: reorganize print statement hiearchy

--- src/main.py
+++ src/main.py
@@ -3,9 +3,26 @@ from src.classes.PageGenerator import PageGenerator
 
 
 def main():
-    FileHandler.copy_files('./static/', './public/')
-    PageGenerator.generate_page(
-        'content/index.md', 'template.html', 'public/index.html'
+    static_dir = input(
+        'Enter PATH of static directory (containing images or CSS files): '
+    )
+
+    dest_dir = input(
+        "Enter PATH of directory where you'd like to place generated files: "
+    )
+
+    FileHandler.copy_files(static_dir, dest_dir)
+
+    content_dir = input(
+        'Enter PATH of content directory (contains markdown files / dirs of markdown files): '
+    )
+
+    template_file = input(
+        'Enter PATH of template file (HTML file that contains {{Title}} and {{Content}}): '
+    )
+
+    PageGenerator.generate_pages_recursive(
+        content_dir, template_file, dest_dir
     )
 
 
